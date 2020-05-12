90b4bd07f55bd2a07b9992944a90cfc256b694c0
crusiris <cruzsirys@gmail.com>
2020-05-12 18:38:38 -0400

    Obteniendo datos del input y guardandolos en el state, Creacion del state para el manejo de errores

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Form = () => {
+
+    //Estado para la busqueda
+    const [ search, saveSearch ] = useState({
+        artista:'',
+        cancion:''
+    });
+
+    //Estado para errores
+
+    const [ error, saveError ]= useState(false);
+
+    //Destructuring
+    const { artista, cancion } = search;
+
+    //Funcion para obtener el contenido de  los input
+    const updateState = e =>{
+        saveSearch({
+            ...search,
+            [e.target.name] : e.target.value
+        })
+    }
+
+    //Peticion get
+    const getInfo = e =>{
+        e.preventDefault();
+
+        if( artista.trim() === '' || cancion.trim() === '' ){
+            saveError(true)
+            return;
+        }
+        saveError(true);
+        
+    }
+    
     return (
         <div className="bg-info">
             <div className="container">
                 <div className="row">
-                    <form className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+                    <form 
+                        onSubmit={getInfo}
+                        className="col card text-white bg-transparent mb-5 pt-5 pb-2">
                         <fieldset>
                             <legend className="text-center">Buscardor letras de Canciones
 
                             <div className="row">
                                 <div className="col-md-6">
+
                                     <div className="form-group">
                                         <label>Artista</label>
                                         <input
@@ -18,19 +55,24 @@ const Form = () => {
                                         className="form-control"
                                         name="artista"
                                         placeholder="Nombre de artista"
-                                        />
+                                        onChange={ updateState }
+                                        value={artista}
+                                       />
                                     </div>
 
                                 </div>
                                 <div className="col-md-6">
+
                                    <div className="form-group">
                                             <label>Canciones</label>
                                             <input
                                             type="text"
                                             className="form-control"
-                                            name="artista"
+                                            name="cancion"
                                             placeholder="Nombre de cancion"
-                                            />
+                                            onChange={ updateState }
+                                            value={cancion}
+                                           />
                                     </div>
                                 </div>
                                 
